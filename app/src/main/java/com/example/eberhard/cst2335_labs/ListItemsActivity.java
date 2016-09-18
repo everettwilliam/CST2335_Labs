4c5086389d8c616e296f4204e85282c8e116795f
everettwilliam <hold0052@algonquinlive.com>
2016-09-18 19:02:01 -0400

    Lab 2 commit Added 2 new activities login and listItems

---
+++ app/src/main/java/com/example/eberhard/cst2335_labs/ListItemsActivity.java
@@ -0,0 +1,15 @@
+package com.example.eberhard.cst2335_labs;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ListItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+
+    }
+
+}
